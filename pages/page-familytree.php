426acd48b30be4b508354e848858c08504b2f3ac
N-5 <yfe63590@gmail.com>
2023-03-06 20:47:03 +0900

    dd

--- pages/page-familytree.php
+++ pages/page-familytree.php
@@ -6,20 +6,16 @@ Template Name: family-Tree
 
 <?php get_header(); ?>
 
-<main id="familytree">
-  <section data-color="dark">
+<main id="vv">
+  <section class="tree" data-color="dark">
     <div class="l-container">
-      <div class="l-container_side">
-        <h2 class="m-h2">
-          <span class="en">Family Tree</span><br>
-          <span class="jp">家系図</span>
-        </h2>
-      </div>
+      <h2 class="m-h2">
+        <span class="en">Family Tree</span><br>
+        <span class="jp">家系図</span>
+      </h2>
 
-      <div class="l-container_content">
-        <div class="c-postarea">
-          ここに内容がはいります。
-        </div>
+      <div class="tree_image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/familytree/familytree.svg" alt="">
       </div>
     </div>
   </section>
